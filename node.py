c626d09b900f67f2fecd7382cd36fc51e3a8c221
Caio Motta <caioangelomotta@gmail.com>
2017-02-06 22:06:35 -0800

    add transistor node

--- node.py
+++ node.py
@@ -2,19 +2,19 @@
 # defines the node classes of the elmore model
 
 
-class elmorenode:
+class ElmoreNode:
 	"""
 		Base class for all RC nodes
 
 	"""
 	def __init__(self, capacitance, resistance, parent, children):
 		"""
-		Creates and instance of an ''elmorenode''
+		Creates and instance of an ''ElmoreNode''
 
 		Args:
 			capacitance (float, double): node capacitance in fF
 			resistance (float, double): node resistance in Ohms
-			parent (elmorenode, gatenode): the parent of the node
+			parent (ElmoreNode, TransitorNode): the parent of the node
 			children (list): the children of the node
 		"""
 		self.capacitance = capacitance
@@ -27,15 +27,44 @@ class elmorenode:
 		else:
 			self.cumCap = None
 			
-
 	def calcptau(self):
 		self.cumCap = capacitance
 		for child in self.children:
 			if (child.cumCap == None):
 				child.calcptau()
 			self.cumCap += child.cumCap
+		self.ptau = resistance*self.cumCap
+
+class TransitorNode:
+	"""
+		Base class for all transistor nodes
+
+	"""
+	def __init__(self, inputCapacitance, outputCapacitance, outputResistance, parents, children):
+		"""
+		Creates and instance of an ''TransitorNode''
+
+		Args:
+			inputCapacitance (float, double): input capacitance in fF
+			outputCapacitance (float, double): output capacitance in fF
+			outputResistance (float, double): output resistance in Ohms (not sure if this is right yet)
+			parent (elmorenode, gatenode): the parent of the node
+			children (list): the children of the node
+		"""
+		self.capacitance = outputCapacitance
+		self.resistance = resistance
+		self.parent = parent
+		self.children = children
+		self.ptau = None
+		self.cumCap = inputCapacitance
+			
+	def calcptau(self):
+		cumCap = capacitance
+		for child in self.children:
+			if (child.cumCap == None):
+				child.calcptau()
+			cumCap += child.cumCap
 		self.ptau = resistance*cumCap
 
 	
 	
-
